bcb90faa61b28be2302fff589ef0837348eb3764
Jaqueline Régis <jlimaregis@gmail.com>
2021-08-16 00:25:19 -0300

    Altera formato dos resultados

--- jogo.py
+++ jogo.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 import random
 
 
@@ -25,9 +26,6 @@ class Jogador:
 
     def __init__(self, personalidade):
         self.personalidade = personalidade
-
-    def __str__(self) -> str:
-        return f"Jogador({self.personalidade}, {self.saldo}, {self.volta}, {self.posicao_anterior}, {self.posicao_atual}, {self.propriedades}, {self.jogando})"
 
     def comprar(self, numero_propriedade, valor_compra, valor_aluguel):
         if self.saldo < valor_compra:
@@ -92,7 +90,6 @@ class Jogador:
     def recebe_aluguel(self, valor_aluguel):
         self.saldo += valor_aluguel
 
-    @property
     def tem_propriedade(self):
         return self.propriedades != None
 
@@ -104,9 +101,6 @@ class Propriedade:
         self.posicao = posicao
         self.proprietario = proprietario
 
-    def __str__(self) -> str:
-        return f"Propriedade({self.valor_venda}, {self.valor_aluguel}, {self.posicao}, {self.proprietario})"
-
     def adiciona_proprietario(self, jogador):
         self.proprietario = jogador
 
@@ -117,104 +111,134 @@ class Propriedade:
         return self.proprietario != None
 
 
-resultados = {
-    "time_out": 0,
-    "turnos_por_partida": 0,
-    "vitorias_por_personalidade": {
-        IMPULSIVO: 0,
-        EXIGENTE: 0,
-        CAUTELOSO: 0,
-        ALEATORIO: 0,
-    },
-}
+def gerar_resultados(resultados):
+    tvpp = resultados["total_de_vitorias_por_personalidade"]
+    pvpp = dict(map(lambda item: (item[0], f"{(item[1]*100)/300:.2f}"), tvpp.items()))
+    mtpp = int(resultados["media_de_turnos_por_partida"] / 300)
+    resultados.update(
+        {
+            "media_de_turnos_por_partida": mtpp,
+            "porcentagem_de_vitorias_por_personalidade": pvpp,
+            "personalidade_com_maior_numeros_de_vitorias": max(
+                tvpp, key=lambda k: tvpp[k]
+            ),
+        },
+    )
+    resultados.pop("total_de_vitorias_por_personalidade")
+    return json.dumps(resultados, sort_keys=True, indent=4)
 
-simulacao = 1
-while simulacao <= SIMULACOES:
-    j1 = Jogador(IMPULSIVO)
-    j2 = Jogador(EXIGENTE)
-    j3 = Jogador(CAUTELOSO)
-    j4 = Jogador(ALEATORIO)
 
-    jogadores = [j1, j2, j3, j4]
-    jogadores = random.sample(jogadores, len(jogadores))
-    perdedores = []
+def jogar():
+    resultados = {
+        "total_de_partidas_terminadas_em_time_out": 0,
+        "media_de_turnos_por_partida": 0,
+        "total_de_vitorias_por_personalidade": {
+            IMPULSIVO: 0,
+            EXIGENTE: 0,
+            CAUTELOSO: 0,
+            ALEATORIO: 0,
+        },
+        "personalidade_com_maior_numeros_de_vitorias": "",
+    }
 
-    tabuleiro = {}
-    for numero in range(1, MAX_DE_CASAS_NO_TABULEIRO + 1):
-        tabuleiro[numero] = Propriedade(
-            VALOR_INICIAL_DO_TABULEIRO * numero,
-            int((VALOR_INICIAL_DO_TABULEIRO * numero) / 2),
-            numero,
-        )
+    simulacao = 1
+    while simulacao <= SIMULACOES:
+        jogadores = [
+            Jogador(IMPULSIVO),
+            Jogador(EXIGENTE),
+            Jogador(CAUTELOSO),
+            Jogador(ALEATORIO),
+        ]
+        jogadores = random.sample(jogadores, len(jogadores))
+        perdedores = []
 
-    rodadas = 1
-    vencedor = False
-    while rodadas < MAX_DE_RODADAS:
-        for jgdr in jogadores:
+        tabuleiro = {}
+        for numero in range(1, MAX_DE_CASAS_NO_TABULEIRO + 1):
+            tabuleiro[numero] = Propriedade(
+                VALOR_INICIAL_DO_TABULEIRO * numero,
+                int((VALOR_INICIAL_DO_TABULEIRO * numero) / 2),
+                numero,
+            )
 
-            jgdr.joga_dado()
-            ppdd = tabuleiro.get(jgdr.posicao_atual)
-            if ppdd.tem_proprietario():
-                if ppdd.proprietario != jgdr:
-                    jgdr.alugar(ppdd.proprietario, ppdd.valor_aluguel)
-            else:
-                jgdr_comprou = jgdr.comprar(
-                    ppdd.posicao, ppdd.valor_venda, ppdd.valor_aluguel
+        rodadas = 1
+        vencedor = False
+        while rodadas < MAX_DE_RODADAS:
+            for jgdr in jogadores:
+
+                jgdr.joga_dado()
+                ppdd = tabuleiro.get(jgdr.posicao_atual)
+                if ppdd.tem_proprietario():
+                    if ppdd.proprietario != jgdr:
+                        jgdr.alugar(ppdd.proprietario, ppdd.valor_aluguel)
+                else:
+                    jgdr_comprou = jgdr.comprar(
+                        ppdd.posicao, ppdd.valor_venda, ppdd.valor_aluguel
+                    )
+                    if jgdr_comprou:
+                        ppdd.adiciona_proprietario(jgdr)
+
+                if not jgdr.jogando:
+                    if jgdr.tem_propriedade():
+                        for posicao_ppdd in jgdr.propriedades:
+                            tabuleiro.get(posicao_ppdd).remove_proprietario()
+                        jgdr.remove_propriedades()
+                        jogadores.remove(jgdr)
+                        perdedores.append(jgdr)
+
+                if len(jogadores) == 1:
+                    vencedor = True
+                    break
+
+            if vencedor and len(jogadores):
+
+                maior_turno = max([j.turno for j in [*jogadores, *perdedores]])
+                resultados.update(
+                    {
+                        "media_de_turnos_por_partida": resultados[
+                            "media_de_turnos_por_partida"
+                        ]
+                        + maior_turno
+                    },
                 )
-                if jgdr_comprou:
-                    ppdd.adiciona_proprietario(jgdr)
-
-            if not jgdr.jogando:
-                if jgdr.tem_propriedade:
-                    for posicao_ppdd in jgdr.propriedades:
-                        tabuleiro.get(posicao_ppdd).remove_proprietario()
-                    jgdr.remove_propriedades()
-                    jogadores.remove(jgdr)
-                    perdedores.append(jgdr)
-
-            if len(jogadores) == 1:
-                vencedor = True
+                resultados["total_de_vitorias_por_personalidade"].update(
+                    {
+                        jogadores[0].personalidade: resultados[
+                            "total_de_vitorias_por_personalidade"
+                        ][jogadores[0].personalidade]
+                        + 1
+                    },
+                )
                 break
 
-        if vencedor and len(jogadores):
+            rodadas += 1
+
+        if rodadas == MAX_DE_RODADAS:
+            if len(jogadores) > 1:
+                vencedor = sorted(jogadores, key=lambda j: (-j.saldo, j.turno))[0]
 
             maior_turno = max([j.turno for j in [*jogadores, *perdedores]])
             resultados.update(
-                {"turnos_por_partida": resultados["turnos_por_partida"] + maior_turno},
-            )
-            resultados["vitorias_por_personalidade"].update(
                 {
-                    jogadores[0].personalidade: resultados[
-                        "vitorias_por_personalidade"
-                    ][jogadores[0].personalidade]
+                    "total_de_partidas_terminadas_em_time_out": resultados["total_de_partidas_terminadas_em_time_out"] + 1,
+                    "media_de_turnos_por_partida": resultados[
+                        "media_de_turnos_por_partida"
+                    ]
+                    + maior_turno,
+                },
+            )
+            resultados["total_de_vitorias_por_personalidade"].update(
+                {
+                    vencedor.personalidade: resultados["total_de_vitorias_por_personalidade"][
+                        vencedor.personalidade
+                    ]
                     + 1
                 },
             )
-            break
 
-        rodadas += 1
+        simulacao += 1
 
-    if rodadas == MAX_DE_RODADAS:
-        if len(jogadores) > 1:
-            vencedor = sorted(jogadores, key=lambda j: (-j.saldo, j.turno))[0]
-
-        maior_turno = max([j.turno for j in [*jogadores, *perdedores]])
-        resultados.update(
-            {
-                "time_out": resultados["time_out"] + 1,
-                "turnos_por_partida": resultados["turnos_por_partida"] + maior_turno,
-            },
-        )
-        resultados["vitorias_por_personalidade"].update(
-            {
-                vencedor.personalidade: resultados["vitorias_por_personalidade"][
-                    vencedor.personalidade
-                ]
-                + 1
-            },
-        )
-
-    simulacao += 1
+    return gerar_resultados(resultados)
 
 
+resultados = jogar()
 print(resultados)
